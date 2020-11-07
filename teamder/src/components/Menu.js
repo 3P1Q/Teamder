f6775e1b76b0fef711f0e3c0ed8241cc94c80c57
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2020-11-07 12:53:31 +0530

    update profile page ui

--- teamder/src/components/Menu.js
+++ teamder/src/components/Menu.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import PersonIcon from '@material-ui/icons/Person';
+import GroupIcon from '@material-ui/icons/Group';
+import EditIcon from '@material-ui/icons/Edit';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 
 const Menu = (props) => {
     return <div>
         <div className="menu">
             <ul>
-                <li>Profile</li>
-                <li>Connect</li>
-                <li>Edit</li>
-                <li>Logout</li>
+                <li><PersonIcon className="icons"/> Profile </li>
+                <li><GroupIcon className="icons"/> Connect</li>
+                <li><EditIcon className="icons"/> Edit</li>
+                <li><PowerSettingsNewIcon className="icons"/> Logout</li>
             </ul>
         </div>
     </div>
